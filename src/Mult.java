b10f896730a89cca71e630a5dbe311d0eea8a48a
Nikolay Veselov <veselov2344@mail.ru>
2019-03-11 20:49:24 +0300

    first

---
+++ src/Mult.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Mult {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println(scan.nextInt()*scan.nextInt());
+    }
+}
